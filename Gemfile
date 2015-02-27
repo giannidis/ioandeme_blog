68442af6891cba5d0277ae8e81d5bd9b730aa109
Giannis Dem <giannidis50@hotmail.com>
2015-02-27 13:47:46 +0200

    Add pictures with paperclip and comments deleted

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'tzinfo-data', platforms: [:mingw, :mswin]
 gem 'devise', '~> 3.3.0'
+gem 'paperclip', '~> 4.2.1'
 
 group :development do
   gem 'sqlite3'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+  has_attached_file :image, styles: { medium: "700x300#"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :body, presence: true
